8bae1f126429936fc26eee62416bc04ac801601c
alhovike <e.alhovik@gmail.com>
2020-12-28 14:46:46 -0500

    2020 day 23 part 2

--- 2020/Day_23/task1.js
+++ 2020/Day_23/task1.js
@@ -1,16 +1,16 @@
 const _ = require('../lib/lodash.min.js');
 
-function pickUp(labeling, current){
+function pickUp(labeling, current) {
     let currentIndex = labeling.findIndex(p => p == current);
-    let pickUpIndex = currentIndex+1 == labeling.length
+    let pickUpIndex = currentIndex + 1 == labeling.length
         ? 0
-        : currentIndex+1;
+        : currentIndex + 1;
     let pickUp = labeling[pickUpIndex]
     labeling.splice(pickUpIndex, 1);
     return pickUp;
 }
-function move(labeling, moves, current, maxValue){
-    if(moves == 0){
+function move(labeling, moves, current, maxValue) {
+    if (moves == 0) {
         return labeling;
     }
     moves--;
@@ -19,21 +19,21 @@ function move(labeling, moves, current, maxValue){
     let pickUp3 = pickUp(labeling, current);
 
     let currentIndex = labeling.findIndex(p => p == current);
-    let newCurrent = labeling[currentIndex+1 == labeling.length?0:currentIndex+1];
+    let newCurrent = labeling[currentIndex + 1 == labeling.length ? 0 : currentIndex + 1];
 
     let destination = -1;
-    while(destination == -1){
+    while (destination == -1) {
         current--;
-        if(current < 0) { current = maxValue; }
+        if (current < 0) { current = maxValue; }
         let destinationIndex = labeling.findIndex(p => p == current);
-        if(destinationIndex >= 0){
+        if (destinationIndex >= 0) {
             destination = current;
-            labeling.splice(destinationIndex+1, 0, pickUp3);
-            labeling.splice(destinationIndex+1, 0, pickUp2);
-            labeling.splice(destinationIndex+1, 0, pickUp1);
+            labeling.splice(destinationIndex + 1, 0, pickUp3);
+            labeling.splice(destinationIndex + 1, 0, pickUp2);
+            labeling.splice(destinationIndex + 1, 0, pickUp1);
         }
     }
-    return move(labeling,moves,newCurrent,maxValue);
+    return move(labeling, moves, newCurrent, maxValue);
 }
 module.exports = function task({ data }) {
     let labeling = data.find(p => true).split('').map(p => +p);
--- 2020/Day_23/task2.js
+++ 2020/Day_23/task2.js
@@ -1,67 +1,93 @@
 const _ = require('../lib/lodash.min.js');
 
-function pickUp(labeling, current){
-    let currentIndex = labeling.indexOf(current);
-    let pickUpIndex = currentIndex+1 == labeling.length
+function createListItem(array) {
+    let head = null;
+    let tail = null;
+    for (let index = 0; index < array.length; index++) {
+        let item = {
+            val: array[index]
+        }
+        if (tail != null) {
+            tail.next = item;
+            item.prev = tail
+        }
+        tail = item;
+        if (index == 0) head = item;
+    }
+    head.prev = tail;
+    tail.next = head;
+
+    return head;
+}
+
+function pickUp(labeling, current, n = 1) {
+    let currentIndex = labeling.findIndex(p => p == current);
+    let pickUpIndex = currentIndex + 1 == labeling.length
         ? 0
-        : currentIndex+1;
-    let pickUp = labeling[pickUpIndex]
-    labeling.splice(pickUpIndex, 1);
-    return pickUp;
+        : currentIndex + 1;
+    let result = labeling.splice(pickUpIndex, n);
+    return result.concat(labeling.splice(0, n - result.length))
 }
-function move(labeling, moves, current, maxValue){
-    if(moves == 0){
-        return labeling;
-    }
-    moves--;
-    let pickUp1 = pickUp(labeling, current);
-    let pickUp2 = pickUp(labeling, current);
-    let pickUp3 = pickUp(labeling, current);
 
-    let currentIndex = labeling.indexOf(current);
-    // let currentIndex = labeling.findIndex(p => p == current);
-    let newCurrent = labeling[currentIndex+1 == labeling.length?0:currentIndex+1];
-
-    let destination = -1;
-    while(destination == -1){
-        current--;
-        if(current < 0) { current = maxValue; }
-        let destinationIndex = labeling.indexOf(current);
-        // let destinationIndex = labeling.findIndex(p => p == current);
-        if(destinationIndex >= 0){
-            destination = current;
-            labeling.splice(destinationIndex+1, 0, pickUp3);
-            labeling.splice(destinationIndex+1, 0, pickUp2);
-            labeling.splice(destinationIndex+1, 0, pickUp1);
-        }
-    }
-    return {
-        moves,newCurrent,maxValue
-    }
-    return move(labeling,moves,newCurrent,maxValue);
+function getMap(head) {
+    let map = {};
+    let current = head;
+    do {
+        map[current.val] = current;
+        current = current.next;
+    } while (current !== head);
+    return map;
 }
+function getArrayFromList(head) {
+    let current = head;
+    let labelingResult = [];
+    do {
+        labelingResult.push(current.val);
+        current = current.next;
+    } while (current !== head);
+    return labelingResult;
+}
+
 module.exports = function task({ data }) {
-    let labeling = data.find(p => true).split('').map(p => +p);
+    let labeling = data[0].split('').map(p => +p);
     let maxValue = Math.max.apply(Math, labeling);
-    Array.from({length: 1000000}, (v, i) => i+1)
+    Array.from({ length: 1000000 }, (v, i) => i + 1)
         .filter(p => p > maxValue)
-        .forEach(p => labeling.push(p))
+        .forEach(p => labeling.push(p));
+    maxValue = 1000000;
 
-    let result = [];
-    let obj = {
-        newCurrent: labeling[0]
+    let head = createListItem(labeling);
+    let map = getMap(head);
+    let current = head;
+    for (let i = 0; i < 10000000; i++) {
+        let pickUp1 = current.next;
+        let pickUp2 = pickUp1.next;
+        let pickUp3 = pickUp2.next;
+
+        let forwardDestination = pickUp3.next;
+
+        current.next = forwardDestination;
+        pickUp1.prev = null;
+
+        forwardDestination.prev = current;
+        pickUp3.next = null;
+
+        let destinationVal = [
+            current.val - 1,
+            current.val - 2,
+            current.val - 3,
+            current.val - 4]
+            .map(val => val < 1 ? maxValue + val : val)
+            .find(p => p != pickUp1.val && p != pickUp2.val && p != pickUp3.val);
+
+        current = current.next;
+        let destination = map[destinationVal];
+        pickUp3.next = destination.next;
+        destination.next.prev = pickUp3;
+
+        destination.next = pickUp1;
+        pickUp1.prev = destination;
     }
-    for(let i=0; i< 10000000; i++){
-        if(i%10000 == 0){
-            console.log(i, new Date)
-        }
-        obj = move(labeling, 10000000, labeling[0], maxValue);
-    }
 
-    return [pickUp(result, 1), pickUp(result, 1)];
-
-
-    // let result = move(labeling, 10000000, labeling[0], maxValue);
-
-    // return [pickUp(result, 1), pickUp(result, 1)];
+    return pickUp(getArrayFromList(head), 1, 2).reduce((a, b) => a * b);
 }
