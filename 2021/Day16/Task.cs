b7062c30d9a11e828ebd8e204c18be7bab1ebdc1
Eugene Alhovik <e.alhovik@gmail.com>
2022-02-22 13:36:59 -0500

    2021 16-17

---
+++ 2021/Day16/Task.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2021.Day16
+{
+    class Task : BaseTask<long>
+    {
+        public override long ExpectedPart1Test { get; set; } = 20;
+        public override long ExpectedPart2Test { get; set; } = 1;
+
+        public override long SolvePart1(IEnumerable<string> input)
+        {
+            var binaryData = string.Join(string.Empty,
+              input.First().Select(
+                c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')
+              )
+            ).ToCharArray();
+
+            var packages = ParsePackages(binaryData).SelectMany(GetAllPackages);
+            return packages
+                .Select(p => p.Version)
+                .Sum();
+        }
+
+        public override long SolvePart2(IEnumerable<string> input)
+        {
+            var binaryData = string.Join(string.Empty,
+              input.First().Select(
+                c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')
+              )
+            ).ToCharArray();
+
+            var packages = ParsePackages(binaryData);
+            if(packages.Count != 1)
+            {
+                throw new Exception();
+            }
+
+            return packages[0].GetValue();
+        }
+
+        private static List<Package> GetAllPackages(Package package)
+        {
+            var result = new List<Package>();
+            result.Add(package);
+            if (package.Packages != null)
+            {
+                foreach (var item in package.Packages)
+                {
+                    result.AddRange(GetAllPackages(item));
+                }
+            }
+            return result;
+        }
+
+        private List<Package> ParsePackages(char[] binaryData)
+        {
+            var result = ParseSubPackages(binaryData, 0, 0, binaryData.Length);
+            return result.Item1;
+        }
+
+        private (List<Package>, int) ParseSubPackages(char[] binaryData, int i, int lengthTypeId, int lengthOfSubPackages)
+        {
+            var subPackages = new List<Package>();
+            int j = i;
+            for (; j < binaryData.Length;)
+            {
+                if (binaryData.Skip(j).Take(3 + 3 + 5).Count() < 11)
+                {
+                    break;
+                }
+
+                var subPackage = new Package();
+                (subPackage.Version, j) = GetValue(j, binaryData, 3);
+                (subPackage.Type, j) = GetValue(j, binaryData, 3);
+                if (subPackage.Type == 4)
+                {
+                    var chunks = new List<string>();
+                    do
+                    {
+                        string chunk;
+                        (chunk, j) = GetBytes(j, binaryData, 5);
+                        chunks.Add(string.Join(string.Empty, chunk.Skip(1)));
+                        if (chunk[0] == '0')
+                        {
+                            break;
+                        }
+                    }
+                    while (true);
+                    subPackage.Value = BinaryToDecimal(string.Join(string.Empty, chunks));
+                }
+                else
+                {
+                    (subPackage.LengthTypeId, j) = GetValue(j, binaryData, 1);
+                    (subPackage.LengthOfSubPackages, j) = GetValue(j, binaryData, subPackage.LengthTypeId == 0 ? 15 : 11);
+
+                    (subPackage.Packages, j) = ParseSubPackages(binaryData, j, subPackage.LengthTypeId, subPackage.LengthOfSubPackages);
+                }
+
+                subPackages.Add(subPackage);
+
+                if (lengthTypeId == 0 && (j - i) >= lengthOfSubPackages)
+                {
+                    break;
+                }
+                else if (lengthTypeId == 1 && subPackages.Count == lengthOfSubPackages)
+                {
+                    break;
+                }
+            }
+
+            return (subPackages, j);
+        }
+
+        private (string, int) GetBytes(int i, char[] binaryData, int length)
+        {
+            var value = string.Join(string.Empty, binaryData.Skip(i).Take(length));
+            return (value, i + length);
+        }
+        private (int, int) GetValue(int i, char[] binaryData, int length)
+        {
+            var value = string.Join(string.Empty, binaryData.Skip(i).Take(length));
+            return ((int)BinaryToDecimal(value), i + length);
+        }
+
+        private static long BinaryToDecimal(string binary)
+        {
+            return Convert.ToInt64(binary, 2);
+        }
+        private class Package
+        {
+            public int Version { get; set; }
+            public int Type { get; set; }
+            public int LengthTypeId { get; set; }
+            public int LengthOfSubPackages { get; set; }
+
+            public long Value { get; set; }
+
+            public List<Package> Packages { get; set; }
+
+            public long GetValue()
+            {
+                var values = Packages != null
+                    ? Packages.Select(p => p.GetValue()).ToList()
+                    : null;
+                switch (Type)
+                {
+                    case 0: return values.Sum();
+                    case 1: return values.Aggregate(1L, (a, b) => a * b);
+                    case 2: return values.Min();
+                    case 3: return values.Max();
+                    case 4: return Value;
+                    case 5: return values[0] > values[1] ? 1 : 0;
+                    case 6: return values[0] < values[1] ? 1 : 0;
+                    case 7: return values[0] == values[1] ? 1 : 0;
+                    default: throw new NotImplementedException();
+                }
+            }
+        }
+    }
+}
---
+++ 2021/Day17/Task.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2021.Day17
+{
+    class Task : BaseTask<int>
+    {
+        private class TargetArea
+        {
+            public int X1 { get; set; }
+            public int X2 { get; set; }
+            public int Y1 { get; set; }
+            public int Y2 { get; set; }
+        }
+        public override int ExpectedPart1Test { get; set; } = 45;
+        public override int ExpectedPart2Test { get; set; } = 112;
+        public override int SolvePart1(IEnumerable<string> input)
+        {
+            var targetArea = input.Select(d => d.Split(": ")[1].Split(", ").Select(p => p.Split("=")[1].Split("..").Select(int.Parse).ToList()))
+                .Select(p => new TargetArea
+                {
+                    X1 = p.ElementAt(0)[0],
+                    X2 = p.ElementAt(0)[1],
+                    Y1 = p.ElementAt(1)[0],
+                    Y2 = p.ElementAt(1)[1]
+                }).First();
+
+            var velocities = BruteForceVelocities(targetArea);
+
+            return velocities.Select(p =>
+            {
+                var yVelocity = p.Item2;
+                var y = 0;
+                var yMax = y;
+                for (int i = 0; i < p.Item3; i++)
+                {
+                    y += yVelocity;
+                    yVelocity -= 1;
+                    yMax = yMax < y ? y : yMax;
+                }
+                return (p, yMax);
+            }).Select(p => p.yMax).Max();
+        }
+
+        public override int SolvePart2(IEnumerable<string> input)
+        {
+            var targetArea = input.Select(d => d.Split(": ")[1].Split(", ").Select(p => p.Split("=")[1].Split("..").Select(int.Parse).ToList()))
+                .Select(p => new TargetArea
+                {
+                    X1 = p.ElementAt(0)[0],
+                    X2 = p.ElementAt(0)[1],
+                    Y1 = p.ElementAt(1)[0],
+                    Y2 = p.ElementAt(1)[1]
+                }).First();
+
+            return BruteForceVelocities(targetArea).Select(p => (p.Item1, p.Item2)).Distinct().Count();
+        }
+
+        private List<(int, int, int)> BruteForceVelocities(TargetArea targetArea)
+        {
+            var xVelocities = new Dictionary<int, List<int>>();
+            for (int x = 0; x < targetArea.X2 + 2; x++)
+            {
+                var xVelocity = x;
+                xVelocities.Add(x, new List<int>() { xVelocity });
+                var prevX = xVelocity;
+                var d = -1;
+                for (int n = 1; n < targetArea.X2; n++)
+                {
+                    if (xVelocity == 0)
+                    {
+                        d = 0;
+                    }
+                    xVelocity += d;
+                    xVelocities[x].Add(prevX += xVelocity);
+                }
+            }
+            xVelocities = xVelocities
+                .Where(p => p.Value.Any(x => targetArea.X1 <= x && x <= targetArea.X2))
+                .ToDictionary(p => p.Key, p => p.Value);
+            var result = new List<(int, int, int)>();
+            for (int yVelocity = targetArea.Y1; yVelocity < targetArea.Y1 + 100000; yVelocity++)
+            {
+
+                for (int y = targetArea.Y1; y <= targetArea.Y2; y++)
+                {
+                    var a = 1;
+                    var b = 1 + 2 * yVelocity;
+                    var c = 2 * y;
+                    var D = b * b - 4 * a * c;
+                    if (D < 0) continue;
+                    var n1 = (-b + Math.Sqrt(D)) / 2 * a;
+                    var n2 = (-b - Math.Sqrt(D)) / 2 * a;
+                    var possibleNs = new List<int?> {
+                    TestY(yVelocity, (int)Math.Abs(n1), y),
+                    TestY(yVelocity, (int)Math.Abs(n2), y)
+                }
+                    .Where(p => p != null)
+                    .ToList();
+                    if (possibleNs.Any())
+                    {
+                        foreach (var n in possibleNs)
+                        {
+                            foreach (var xVelocity in xVelocities)
+                            {
+                                if (xVelocity.Value.Count >= n.Value)
+                                {
+                                    var x = xVelocity.Value[n.Value - 1];
+                                    if (targetArea.X1 <= x && x <= targetArea.X2)
+                                    {
+                                        result.Add((xVelocity.Key, yVelocity, n.Value));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        private int? TestY(int yVelocity, int n, int expectedY)
+        {
+            return (2 * yVelocity + (-1) * (n - 1)) * n / 2 == expectedY
+                ? n
+                : (int?)null;
+        }
+    }
+}
